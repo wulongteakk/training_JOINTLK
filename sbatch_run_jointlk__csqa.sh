31cedb719a5e67013d8e658d04a3a7bf8f14e546
June <884123737@qq.com>
2025-11-05 21:14:41 +0800

    上传原jointlk相关

---
+++ sbatch_run_jointlk__csqa.sh
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<meta id="viewport" name="viewport" content="target-densitydpi=device-dpi,width=640px,user-scalable=1">
+<meta name="renderer" content="webkit" />
+<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE">
+<title>上网登录页</title> 
+<!--Dr.COMWebLoginID_0.htm-->
+
+<script type="text/javascript">
+sv=1;sv1=0;v6='http://[2001:da8:c800:ffff::f012]:9002/v6               ';myv6ip='                                       ';v4serip='10.10.8.162'    ;m46=0;v46ip='172.18.76.110'                          ;
+vid=0   ;mip=172018076110;Gno=0000;vlanid="0"   ;AC="";                          ipm="0a0a08a2";ss1="0010f380ee92";ss2="0000";ss3="ac124c6e";ss4="000000000000";ss5="172.18.76.110"  ;ss6="10.10.8.162"    ;timet=1762146113; 
+osele=0;//1=不弹窗
+domain='[2001:da8:c800:ffff::f012]';// //////////////////
+zxopt=0;//1为普通，8为直通(需201606后的内核)
+hidm=0,hidn=0;/////
+aolno=19747;//////
+wopt=0;
+eport=-1,eclass=1;//
+</script>
+
+<script type="text/javascript">
+/** 哆点参数 start */
+//1.基本参数
+portalid='';//门户ID
+portalname='';//门户名称
+//2.可选参数
+portalver='0050000000';//门户版本
+serialno='';//序列号
+logourl='';//门户logo
+bannerurl='';//横幅广告
+welcome='';//欢迎词
+businessurl='';//业务接口
+//3.旁路参数
+authexenable='1';//是否启用旁路扩展模式
+authtype=1;//登录协议
+authloginIP='';//登录IP
+authloginport=801;//登录端口
+authloginpath='/eportal/?c=ACSetting&a=Login';//登录路径
+authloginparam='url=drappall'; //登录参数
+authuserfield='DDDDD';//账号节点
+authpassfield='upass';//密码节点
+terminalidentity=1;//终端识别标识 先配置成填写
+authlogouttype=1;//注销协议
+authlogoutIP='';//注销IP
+authlogoutport=801;//注销端口
+authlogoutpath='/eportal/?c=ACSetting&a=Logout&ver=1.0';//注销路径
+authlogoutparam='url=drappall';//注销参数
+authlogoutpost='';//注销post参数
+querydelay=0;//登录后延时查询网络状态
+querytype=1;//状态查询协议
+queryIP='';//状态查询IP	
+queryport=80;//状态查询端口
+querypost='';//状态查询post参数
